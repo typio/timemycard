ac576164bea78d731a106aa141667b65730bde3e
Thomas Huber <me@tohuber.com>
2023-11-23 01:00:44 -0700

    🔖Version 2.0.0 in Vue

--- src/stores/calculatorStore.ts
+++ src/stores/calculatorStore.ts
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 
 export enum AMPM {
     am = 'am',
@@ -22,17 +22,28 @@ export type TimeCard = {
 
 export type Settings = {
     firstDay: number,
-    h12: boolean,
+    dayNames: number,
+    h24: boolean,
     hasOT: boolean,
     OTPeriod: 'day' | 'week' | 'both' | 'california',
     OTHours: number,
     minHoursPerDay: number,
 }
 
-export const calculatorStore = reactive<{
-    timeCards: [TimeCard],
-    settings: Settings
-}>({
+const STORAGE_KEY = 'calculatorAppState';
+
+function saveState(state) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
+
+function loadState() {
+    const savedState = localStorage.getItem(STORAGE_KEY);
+    return savedState ? JSON.parse(savedState) : null;
+}
+
+const initialState = loadState() ?? {
+    name: '',
+    date: '',
     timeCards: Array.from({ length: 1 }, () => {
         return {
             days: Array.from({ length: 5 }, () => {
@@ -46,10 +57,66 @@ export const calculatorStore = reactive<{
 
     settings: {
         firstDay: 0,
-        h12: false,
+        dayNames: 0,
+        h24: false,
         hasOT: true,
         OTPeriod: 'day',
         OTHours: 8,
         minHoursPerDay: 0,
     },
-})
+}
+
+export const calculatorStore = reactive(initialState);
+
+const addWeek = (store) => {
+    store.timeCards.push({
+        days: Array.from({ length: store.timeCards?.at(-1)?.days?.length ?? 5 }, () => ({
+            in: { hours: undefined, minutes: undefined, amPm: AMPM.am },
+            out: { hours: undefined, minutes: undefined, amPm: AMPM.pm },
+        })),
+    });
+}
+
+const removeWeek = (store) => {
+    if (store.timeCards.length > 1)
+        store.timeCards.pop();
+}
+
+const clearFields = (store) => {
+    store.timeCards.forEach(card => card.days.forEach(day => {
+        day.in = { hours: undefined, minutes: undefined, amPm: AMPM.am };
+        day.out = { hours: undefined, minutes: undefined, amPm: AMPM.pm };
+    }));
+    store.name = ''
+}
+
+const setDaysPerWeek = (store, dayAmount: number) => {
+    if (dayAmount < 1 || dayAmount > 7) return; // Prevent setting days to less than 1
+
+    store.timeCards.forEach(card => {
+        const currentLength = card.days.length;
+
+        if (dayAmount > currentLength) {
+            for (let i = currentLength; i < dayAmount; i++) {
+                card.days.push({
+                    in: { hours: undefined, minutes: undefined, amPm: AMPM.am },
+                    out: { hours: undefined, minutes: undefined, amPm: AMPM.pm },
+                });
+            }
+        } else if (dayAmount < currentLength) {
+            card.days.length = dayAmount;
+        }
+    });
+}
+
+
+calculatorStore.addWeek = () => addWeek(calculatorStore);
+calculatorStore.removeWeek = () => removeWeek(calculatorStore);
+calculatorStore.clearFields = () => clearFields(calculatorStore);
+calculatorStore.setDaysPerWeek = (dayAmount) => setDaysPerWeek(calculatorStore, dayAmount);
+
+
+
+watch(calculatorStore, (newState) => {
+    saveState(newState);
+}, { deep: true });
