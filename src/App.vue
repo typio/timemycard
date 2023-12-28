fa28f6b901f15b4e91a6c54772886c7b1f6723a3
Thomas Huber <me@tohuber.com>
2023-12-28 13:47:31 -0700

    🎨 Fix footer overflow and simplify

--- src/App.vue
+++ src/App.vue
@@ -37,22 +37,11 @@ import TimeCardList from './components/TimeCardList.vue'
     </div>
   </div>
 
-  <footer class=" text-sm mt-12 py-6 px-4  font-serif border-t-[1px] w-[100vw]">
-    <div class="max-w-5xl mx-auto flex flex-col md:flex-row place-items-center md:place-items-end place-content-between">
-      <p>Contact <a tabindex="-1" href="mailto:me@tohuber.com"
-          class="hover:underline underline-offset-2">me@tohuber.com</a> for
-        questions.</p>
-
-      <div class="flex flex-col place-items-center md:place-items-end">
-        <a tabindex="-1" class="my-4 md:mb-1 hover:underline underline-offset-2 "
-          href="https://github.com/typio/time-my-card">Source
-          code</a>
-        <p>
-          © {{ Math.max(new Date().getFullYear(), 2023) }} by <a tabindex="-1" href="https://tohuber.com"
+  <footer class="text-sm mt-12 py-6 px-4  font-serif border-t-[1px] w-full">
+        <p class="text-center">
+          © {{ Math.max(new Date().getFullYear(), 2023) }} <a tabindex="-1" href="https://tohuber.com"
             class="hover:underline underline-offset-2">Thomas Huber</a>
         </p>
-      </div>
-    </div>
 
   </footer>
 </template>
